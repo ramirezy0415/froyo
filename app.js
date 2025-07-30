fcca94bf391c5a6b95994c25a1dcad402a301366
Yesenia Ramirez <ramirezy0415@gmail.com>
2025-07-29 22:13:39 -0500

    feat(documentation): Including code documentation

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
+// Prompt the user to provide a comma-separated list of froyo flavors
 const user_flavors = prompt(
   "Please enter a list of comma-separated froyo flavors. Example:vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 );
 
-function count_flavor_frequency(flavors) {
+/**
+ * Counts the frequency of each flavor in the string provided
+ *
+ * @param {flavors} string - A comma-separated string of flavors
+ * @returns {hashmap} Returns a hashmap containing the frequency counts of all flavors
+ */
+function flavor_frequency_counter(flavors) {
+  // Derive a list of the flavors provided by splitting on the comma
   const flavor_list = flavors.split(",");
   let flavor_counter = {};
+
+  // Iterate through the flavors in the list and insert into the hashmap
   for (let index = 0; index < flavor_list.length; index++) {
     const flavor = flavor_list[index];
+    // If flavor is in counter, increase the count
     if (flavor in flavor_counter) {
       flavor_counter[flavor]++;
     } else {
+      // If flavor not in counter, initialize to 1
       flavor_counter[flavor] = 1;
     }
   }
+  // Return counter results
   return flavor_counter;
 }
 
-console.log(count_flavor_frequency(user_flavors));
+console.log(flavor_frequency_counter(user_flavors));
